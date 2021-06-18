5fa85e5f8df2005aee692e2842a0ec907f794920
Nobal Dhruw <nobal.kishor@elucidata.io>
2021-06-18 14:04:47 +0530

    initial commit

--- app.R
+++ app.R
@@ -1,9 +1,34 @@
 library(shiny)
 library(shinydashboard)
 
-header <- dashboardHeader()
-sidebar <- dashboardSidebar()
-body <- dashboardBody()
+# Give a title to your app here
+header <- dashboardHeader(
+    title = "Differential Analysis"
+)
+
+# Put all the menu items in the sidebar
+sidebar <- dashboardSidebar(
+    sidebarMenu(
+        menuItem("Data", tabName = "input_data"),
+        menuItem("Viz", tabName = "visualization")
+    )
+)
+
+# Main panel where all the action happens
+body <- dashboardBody(
+    tabItems(
+        tabItem(
+            tabName = "input_data",
+            h1("Input data"),
+            p("This tab will display the data")
+        ),
+        tabItem(
+            tabName = "visualization",
+            h1("Visualization"),
+            p("This tab will have some graphs/plots")
+        )
+    )
+)
 
 ui <- dashboardPage(
     header = header,
